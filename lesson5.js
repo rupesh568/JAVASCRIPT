51a295030a10411cdf5bf014f2f897864b84182b
Rupesh568 <rupeshjaiswal999939@gmail.com>
2025-06-25 15:17:33 +0530

    lesson5-arrow function basic

--- lesson5.js
+++ lesson5.js
@@ -17,9 +17,26 @@ myMessage("I love JS");
 
 
 //sum of two numbers using function:
-function sumNumber(num1,num2){
+function sumNumber(num1,num2){ //parameters are local variable means it is alive within the block of the funtion only but not oustside of it.
     sum=num1+num2;
+    // console.log(num1);
     return sum;
 }
 let result=sumNumber(2,3);
 console.log(`The sum is ${result}`);
+
+//product of two vaiable;
+function productNum(a,b){
+    return a*b;
+}
+let relsult2=productNum(3,4);
+console.log(`The product is ${relsult2}`);
+
+
+//Arrow funtion:It is the simplest form of writing of function:
+
+const sumNum1=(x,y)=>{
+    return x+y;
+}
+let result3=sumNum1(4,4);
+console.log(result3);
