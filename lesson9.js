268adbbdee306e4cd997d12fa8505c7e624be6d5
Rupesh568 <rupeshjaiswal999939@gmail.com>
2025-07-02 11:40:30 +0530

    completed lesson9:DOM PART-3

--- lesson9.js
+++ lesson9.js
@@ -9,11 +9,54 @@
 
 let div=document.querySelector("div");
 console.log(div.getAttribute("id"));
-div.setAttribute("id","one");
-console.log(div.getAttribute("id"));
+// div.setAttribute("id","one");
+// console.log(div.getAttribute("id"));
 
 //for paragraph ;
 let para=document.querySelector("p");
 console.log(para.getAttribute("class"));
 para.setAttribute("class","newClass");
 console.log(para.getAttribute("class"));
+
+//3.Changing style of css or html using js code:
+let div2=document.querySelector("div");
+div2.style.backgroundColor="blue";
+div2.style.fontSize="30px";
+
+
+//DOM MANIPULSTION:
+//INSERT ELEMENTS WITH THE HELP OF JS;
+//.node.append(el) //adds at the end of the node(inside)
+//.node.prepend(el) //adds at the start of node(inside)
+//.node.before(el) //adds before teh node(outside)
+//.node.after(el) //adds after the node(outside)
+
+//Delete Element:
+//node.remove() //
+
+//we have added a new button using js;
+
+let newButton=document.createElement("button");
+newButton.innerText="Click";
+console.log(newButton);
+console.log(newButton.innerText);
+newButton.className="buttonclass"; //given class name to set its property in css;
+
+let div3=document.querySelector("div");
+div3.append(newButton); //button comes at the last of the dive inside div:
+div3.prepend(newButton);//button comes at the start of the dive inside div:
+div3.before(newButton);//button comes before the div
+div3.after(newButton);//button comes after the div
+
+let newHeading=document.createElement("h1");
+newHeading.innerHTML="<i>I am Rupesh Kumar Chaudhary</i>";
+console.log(newHeading);
+
+document.querySelector("body").prepend(newHeading);
+
+let parg1=document.querySelector("p");
+parg1.remove();
+
+
+
+
