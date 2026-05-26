91f42f0968a62120e821d879641e8418bd70daa3
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-05-26 10:24:18 +0530

    apicall-async await

--- lesson16.js
+++ lesson16.js
@@ -1,1 +1,27 @@
+let URL="https://jsonplaceholder.typicode.com/posts";
+let promise=fetch(URL);
+console.log(promise);
+let btn=document.createElement("button");
+btn.innerText="click me!";
+document.body.prepend(btn);
 
+const getData=async()=>{
+    console.log("getting data.....");
+    let response=await fetch(URL);
+    console.log(response);
+    console.log(response.status);
+    console.log(response.type);
+
+    let data=await response.json();
+    console.log(data[0].body);
+    let para=document.querySelector("#para");
+    para.innerText=data[0].body
+    console.log(data[1].body);
+    btn.addEventListener("click",(e)=>{
+        console.log(data[1].body);
+    })
+}
+getData();
+
+// let para=document.querySelector("#para");
+// para.innerText=data[0].body
